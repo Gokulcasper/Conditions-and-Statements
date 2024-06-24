901b9971358b4a021408b7aff0f15a982090cb0b
Gokulcasper <ronitcasper215@gmail.com>
2024-06-24 22:08:28 +0530

    Learned For Loop condition

--- style.js
+++ style.js
@@ -30,39 +30,69 @@
 // }
 
 //Switch Statements
-let grade = "A";
-switch(grade){
-    case "A":
-    case "A+":
-        console.log("Super Grade");
-        break;
-    case "E":
-        console.log("Excellent Grade");
-        break;
-    case "P":
-        console.log("Just Pass");
-        break;
-    case "F":
-        console.log("Fail");
-        break;
-    default:
-        console.log("unknown grade");
-}
+// let grade = "A";
+// switch(grade){
+//     case "A":
+//     case "A+":
+//         console.log("Super Grade");
+//         break;
+//     case "E":
+//         console.log("Excellent Grade");
+//         break;
+//     case "P":
+//         console.log("Just Pass");
+//         break;
+//     case "F":
+//         console.log("Fail");
+//         break;
+//     default:
+//         console.log("unknown grade");
+// }
 //Give the value by the Order High to Low
-let mark = 950;
-switch(true){
-    case mark > 90:
-        console.log("Excellent");
-        break;
-    case  mark > 70:
-        console.log("Good");
-        break;
-    case  mark > 40:
-        console.log("Just Pass");
-        break;
-    case  mark < 40:
-        console.log("Fail");
-        break;
-    default:
-        console.log("unknown grade");
+// let mark = 950;
+// switch(true){
+//     case mark > 90:
+//         console.log("Excellent");
+//         break;
+//     case  mark > 70:
+//         console.log("Good");
+//         break;
+//     case  mark > 40:
+//         console.log("Just Pass");
+//         break;
+//     case  mark < 40:
+//         console.log("Fail");
+//         break;
+//     default:
+//         console.log("unknown grade");
+// }
+// let mobile = 7000;
+// switch(true){
+//     case  mobile > 60000:
+//         console.log("Buy Iphone");
+//         break;
+//     case mobile > 10000:
+//         console.log("Buy Android Mobile");
+//         break;
+//     case  mobile > 5000:
+//     case  mobile < 10000:
+//         console.log("Buy Basic Mobile");
+//         break;
+//     default:
+//         console.log("You Can't afford mobile phone Now!");
+// }
+
+// Looping Conditions
+// for(let i=1;i<=5;i++){
+//     console.log("Number #", i);
+// }
+// for(let i=1;i<=5;i++){
+//     if(i % 2 !== 0){
+//         console.log("Number #" + i);
+//     }
+// }
+for(let i = 10; i >= 1; i-- ){
+    if(i % 2 !== 0){
+        console.log("Odd Number #" + i);
+    }
 }
